58a6fd7d23c6c1e429feca8fee45c6ce669c0160
baybled <31955015+baybled@users.noreply.github.com>
2018-08-17 15:24:41 +0100

    Form now works

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
+from flask import request
+from flask import url_for
 from flask_bootstrap import Bootstrap
 import sys
 from user import User
@@ -8,25 +11,18 @@ from user import User
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-@app.errorhandler(404)
-def page_not_found(e):
-	return render_template('404.html', e=e), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-	return render_template('500.html', e=e), 500
-
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/<term>')
-def query(term):
-	try:
+@app.route('/query', methods=['GET', 'POST'])
+def query():
+	if request.method == 'POST': 
+		term = request.form['term']
 		user = User(term)
-		return render_template('query.html', url=user.url, pic=user.pic, name=user.name, description=user.description, tweet=user.tweet, hashtags=user.hashtags(), remaining=user.remaining())
-	except Exception as e:
-		internal_server_error(e)
+		return render_template('query.html', term=term, url=user.url, pic=user.pic, name=user.name, description=user.description, tweet=user.tweet, hashtags=user.hashtags(), remaining=user.remaining())
+	else:
+		return redirect(url_for('index'))
 
 if __name__=="__main__":
 
