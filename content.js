888268dfb83957c638438afd29f7ba76cab5431d
baybled <baybled@hotmail.com>
2018-09-13 12:46:02 +0100

    added logic

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
 console.log('This test is working')
+// object that fetches and tallies
+
+function logic() {
+	let bio = Document.getElementByClass('ProfileHeaderCar-bio')
+	console.log(bio.innerHTML)
+}
