c2b384320c5e36710fd624f68161a500b7b46738
baybled <baybled@hotmail.com>
2018-09-13 13:02:33 +0100

    Test of logic

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ console.log('This test is working');
 function logic() {
 	let bio = document.getElementsByClassName('ProfileHeaderCard-bio')[0];
 	console.log(bio.innerHTML);
-	bio.innerHTML += "this is again a new test"
+	bio.innerHTML += "<br><br>I mostly use these tags"
 };
 
 logic();
