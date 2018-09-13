cb25a3809276fe8c432441ebd58814d072d381df
baybled <baybled@hotmail.com>
2018-09-13 12:49:47 +0100

    Remembered to call function

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
-console.log('This test is working')
+console.log('This test is working');
 // object that fetches and tallies
 
 function logic() {
 	let bio = document.getElementByClassName('ProfileHeaderCar-bio')
 	console.log(bio.innerHTML)
-}
+};
+
+logic();
