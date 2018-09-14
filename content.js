3868ae674a596e9926558ffa2cec5cc57bb9d689
baybled <baybled@hotmail.com>
2018-09-14 16:12:41 +0100

    Added sorting function

--- content.js
+++ content.js
@@ -1,17 +1,53 @@
-// object that fetches and tallies
+/* Finds hashtags on page and return stop three */
 
-function logic(your_text_here) {
+function addBio(your_text_here) {
+	// finds the bio
 	let bio = document.getElementsByClassName('ProfileHeaderCard-bio')[0];
-	console.log(bio.innerHTML);
+
+	// adds text
 	bio.innerHTML += '<br>' + your_text_here;
 };
 
-let hashtags = document.getElementsByClassName('twitter-hashtag');
+function findTags () {
 
-let pattern = RegExp('<b>(.+)</b>')
+	// finds all hashtags on page
+	let hashtags = document.getElementsByClassName('twitter-hashtag');
 
-for (one in hashtags) {
+	// extracts it into array 
+	let pattern = RegExp('<b>(.+)</b>');
 
-	let match = pattern.exec(hashtags[one].innerHTML)
-	logic(match[0])
+	let matches = [];
+
+	for (one in hashtags) {
+
+		match.push(pattern.exec(hashtags[one].innerHTML));
+
+	return matches;
+
+	}
+
+function sortByFrequency(array) {
+	var frequency = {};
+	// set all initial frequencies for each word to zero
+	array.forEach(
+		function(value) { frequency[value] = 0; });
+
+	// create new array with words and their frequencies
+	var uniques = array.filter(
+		function(value) { return ++frequency[value] == 1; });
+
+	// sort words by abc order
+	return uniques.sort(
+		function(a, b) { return frequency[b] - frequency[a]; });
+};
+
+function main() {
+
+	let tags = findTags();
+
+	sortByFrequency(tags);
+
+	console.log(tags);
+
+	addBio('I mostly use' + tags[2] + ', ' tags[1] + ', ' + 'and ' + tags[0])
 }
