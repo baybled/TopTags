955793a7f75f777b2413a83d886d3616418e9835
baybled <baybled@hotmail.com>
2018-09-14 14:45:35 +0100

    Test to pick up hashtags

--- content.js
+++ content.js
@@ -1,10 +1,13 @@
-console.log('This test is working');
 // object that fetches and tallies
 
-function logic() {
+function logic(your_text_here) {
 	let bio = document.getElementsByClassName('ProfileHeaderCard-bio')[0];
 	console.log(bio.innerHTML);
-	bio.innerHTML += "<br><br>I mostly use these tags"
+	bio.innerHTML += your_text_here;
 };
 
-logic();
+let hashtags = document.getElementsByClassName('twitter-hashtag')
+
+logic('<br><br>this test is working');
+
+console.log(hashtags[0].innerHTML)
